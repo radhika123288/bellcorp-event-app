23405ba0db28e009816cc5dcdb10d73db79f7733
Radhi <sirasalaradhika1@gmail.com>
2026-02-16 20:57:36 +0530

    Fixed port issue

--- server/server.js
+++ server/server.js
@@ -17,9 +17,11 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
-    });
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 
   })
   .catch((err) => {
